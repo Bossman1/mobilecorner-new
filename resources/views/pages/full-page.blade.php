8f33e20a84d296b47a264ce9291d46894bfd54ae
Bossman1 <nikakharadze@yahoo.com>
2025-11-25 12:14:08 +0400

    sidebar resolution for laptop 1440p

--- resources/views/pages/full-page.blade.php
+++ resources/views/pages/full-page.blade.php
@@ -14,7 +14,7 @@
     <div class="container mx-auto font-custom-regular">
 
         <x-page-component position="right" class="my-[20px] !gap-[20px]"
-                          sidebar-class="bg-[var(--color-second-header)] rounded-md xl:!w-[414px] sidebar-content-class transition-all duration-300 mt-[20px]">
+                              sidebar-class="bg-[var(--color-second-header)] rounded-md xl:!w-[350px] 2xl:!w-[414px] sidebar-content-class transition-all duration-300 mt-[20px]">
             <x-slot:content>
 
                 @php
