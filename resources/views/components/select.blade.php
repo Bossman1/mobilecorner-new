1261b4c00be45d9c326ee25c8e7bb0209c26fd9b
Bossman1 <nikakharadze@yahoo.com>
2025-12-14 01:41:19 +0400

    update component select, add possibility add nested group  select , set to checkout page all rtegions and cities of georgia

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -27,10 +27,31 @@
   "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 dark:text-neutral-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
 }' class="hidden">
     <option value="">{{ $placeholder }}</option>
-    @if(is_array($options) && count($options) > 0)
-        @foreach($options as $k => $option)
-            <option value="{{ $k }}" @selected($selected == $k)>{{ $option }}</option>
+
+    @if(is_array($options) && count($options))
+        @foreach($options as $key => $value)
+
+            {{-- GROUP HEADER --}}
+            @if(is_array($value))
+                <option disabled class="font-semibold text-slate-500 bg-slate-50 cursor-default">
+                    {{ $key }}
+                </option>
+
+                @foreach($value as $k => $v)
+                    <option value="{{ $k }}" @selected($selected == $k)>
+                        &nbsp;&nbsp;{{ $v }}
+                    </option>
+                @endforeach
+
+                {{-- SIMPLE OPTIONS --}}
+            @else
+                <option value="{{ $key }}" @selected($selected == $key)>
+                    {{ $value }}
+                </option>
+            @endif
+
         @endforeach
     @endif
+
 </select>
 <!-- End Select -->
